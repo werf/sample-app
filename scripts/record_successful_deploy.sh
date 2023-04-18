78176ff61ad6548f2c2c8324fe51b05efdaa35a5
Timofey Kirillov <timofey.kirillov@flant.com>
2023-04-18 15:09:47 +0300

    ++

--- scripts/record_successful_deploy.sh
+++ scripts/record_successful_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./scripts/prepare_successful_deploy.sh
-./scripts/_record.sh ./scripts/run_successful_deploy.sh
+./scripts/_record.sh ./scripts/run_successful_deploy.sh assets/successful_deploy.cast --overwrite
