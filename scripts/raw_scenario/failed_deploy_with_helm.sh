3ddd689536caadb79b630d0843ff323dc2d728dd
Timofey Kirillov <timofey.kirillov@flant.com>
2023-04-18 20:37:20 +0300

    ++

--- scripts/raw_scenario/failed_deploy_with_helm.sh
+++ scripts/raw_scenario/failed_deploy_with_helm.sh
@@ -7,7 +7,7 @@ kubectl -n sample-app-2 describe pod $(kubectl -n sample-app-2 get pod | grep Cr
 kubectl -n sample-app-2 logs $(kubectl -n sample-app-2 get pod | grep Crash | cut -d' ' -f1)
 # let's check out deployment manifest
 cat .helm/templates/deployment.yaml
-# typo in the image field, let's fix it
+# typo in the command field, let's fix it
 sed -e 's|"star"|"start"|' -i .helm/templates/deployment.yaml
 helm upgrade --wait --install --create-namespace --namespace sample-app-2 sample-app .helm/ --set werf.image.app=${APP_IMAGE} --set host=sample-app-2.test --timeout 60s
 curl sample-app.test/ping
