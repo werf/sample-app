cdd911149b34bf82bde353d3a21fe94d528f0f2d
Timofey Kirillov <timofey.kirillov@flant.com>
2023-04-18 19:48:03 +0300

    ++

--- scripts/run_failed_deploy.sh
+++ scripts/run_failed_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 source scripts/env.sh
-export WERF_DEV=1
 
-tmux new-session -d "INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
+tmux new-session -d "source scripts/raw_scenario/introduce_error.sh ; INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
      split-window -h "INPUT_THROTTLING=18 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
      attach
