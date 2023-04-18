ae7f35855a701f9d22f53bfaaa7cb3848b737333
Timofey Kirillov <timofey.kirillov@flant.com>
2023-04-18 14:24:30 +0300

    ++

--- scripts/raw_scenario/successful_deploy_with_helm.sh
+++ scripts/raw_scenario/successful_deploy_with_helm.sh
@@ -1,2 +1,3 @@
+echo ${APP_IMAGE}
 helm upgrade --wait --install --create-namespace --namespace sample-app-2 sample-app .helm/ --set werf.image.app=${APP_IMAGE} --set host=sample-app-2.test
 curl sample-app-2.test/ping
