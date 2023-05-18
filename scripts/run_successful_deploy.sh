d6a5ee2cbc0a8e83f68110b00f3b16d029ad8ebf
Timofey Kirillov <timofey.kirillov@flant.com>
2023-05-18 15:03:33 +0300

    fix(demo): switch helm and werf sides in demo

--- scripts/run_successful_deploy.sh
+++ scripts/run_successful_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tmux new-session -d "source scripts/env.sh ; INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/successful_deploy_with_werf.sh ; bash" \; \
-     split-window -h "source scripts/env.sh ; INPUT_THROTTLING=18 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source scripts/_type_and_execute.sh scripts/raw_scenario/successful_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
+tmux new-session  -d "source scripts/env.sh ; INPUT_THROTTLING=18 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source scripts/_type_and_execute.sh scripts/raw_scenario/successful_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
+     split-window -h "source scripts/env.sh ; INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/successful_deploy_with_werf.sh ; bash" \; \
      attach
