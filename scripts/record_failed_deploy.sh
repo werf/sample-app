411e1926f95a10ebfbb9a6f81717dad4990c4ab8
Timofey Kirillov <timofey.kirillov@flant.com>
2023-05-18 17:08:49 +0300

    chore(scripts)

--- scripts/record_failed_deploy.sh
+++ scripts/record_failed_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./scripts/prepare_failed_deploy.sh
-./scripts/_record.sh --command ./scripts/run_failed_deploy.sh /tmp/failed_deploy.cast --overwrite
+./scripts/_record.sh --command ./scripts/run_failed_deploy.sh assets/failed_deploy.cast --overwrite
