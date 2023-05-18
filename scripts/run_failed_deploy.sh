b235ee19bbac3ea42ba99e1a2d3ee31040a9024f
Timofey Kirillov <timofey.kirillov@flant.com>
2023-05-18 17:18:30 +0300

    chore(scripts)

--- scripts/run_failed_deploy.sh
+++ scripts/run_failed_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tmux new-session  -d "source scripts/env.sh ; INPUT_THROTTLING=18 APP_IMAGE=$(cat /tmp/images.json | jq -r .Images.app.DockerImageName) source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_helm.sh ${APP_IMAGE} ; bash" \; \
-     split-window -h "source scripts/env.sh ; WERF_REQUIRE_BUILT_IMAGES=1 INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
+     split-window -h -l 65% "source scripts/env.sh ; WERF_LOG_TERMINAL_WIDTH=90 WERF_REQUIRE_BUILT_IMAGES=1 INPUT_THROTTLING=11 source scripts/_type_and_execute.sh scripts/raw_scenario/failed_deploy_with_werf.sh ; bash" \; \
      attach
