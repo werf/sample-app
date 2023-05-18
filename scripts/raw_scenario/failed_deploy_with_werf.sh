5dc8b67f672723f775a2f9732b84d02e6eeeffbe
Timofey Kirillov <timofey.kirillov@flant.com>
2023-05-18 15:04:55 +0300

    fix(demo): utilize werf's new --require-built-images flag

--- scripts/raw_scenario/failed_deploy_with_werf.sh
+++ scripts/raw_scenario/failed_deploy_with_werf.sh
@@ -1,8 +1,8 @@
-werf converge --repo k3d-registry.sample-app.test:5000/sample-app
+werf converge --require-built-images --repo k3d-registry.sample-app.test:5000/sample-app
 # seems we have a broken pod, which cannot start
 # let's check out deployment manifest
 cat .helm/templates/deployment.yaml
 # typo in the command field, let's fix it
 sed -e 's|"star"|"start"|' -i .helm/templates/deployment.yaml
-werf converge --repo k3d-registry.sample-app.test:5000/sample-app
+werf converge --require-built-images --repo k3d-registry.sample-app.test:5000/sample-app
 curl sample-app.test/ping
